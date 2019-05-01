f75f31437ac2f6857db2f72e942d7919d949e45b
Duban <mr.garcialml@gmail.com>
2019-05-01 14:58:10 -0500

    crear test

---
+++ Metodossimulacion1.0/src/test/TestLinearCongruency.java
@@ -0,0 +1,12 @@
+package test;
+
+import views.JDialogLinealCongruency;
+
+public class TestLinearCongruency {
+	
+	public static void main(String[] args) {
+		JDialogLinealCongruency dialogLinealCongruency = new JDialogLinealCongruency();
+		dialogLinealCongruency.setVisible(true);
+	}
+
+}
--- Metodossimulacion1.0/src/views/JDialogLinealCongruency.java
+++ Metodossimulacion1.0/src/views/JDialogLinealCongruency.java
@@ -1,8 +1,39 @@
 package views;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 public class JDialogLinealCongruency extends JDialog{
+	
+	private MyJTextField jTextFieldX0, jTextFieldK, jTextFieldG, jTextFieldC;
+	
+	public JDialogLinealCongruency() {
+		this.setSize(new Dimension((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth()/3,
+				(int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight()- 
+						Toolkit.getDefaultToolkit().getScreenSize().getHeight()/4)));
+		this.setLocationRelativeTo(null);
+		
+		init();
+	}
+
+	private void init() {
+		jTextFieldX0 = new MyJTextField("Xo");
+		jTextFieldK = new MyJTextField("K");
+		jTextFieldG = new MyJTextField("G");
+		jTextFieldC = new MyJTextField("C");
+		
+		JPanel jPanel1 = new JPanel(new GridLayout(2,2,10,10));
+		
+		
+		
+		
+	}
+	
+	
 	
 	
 
