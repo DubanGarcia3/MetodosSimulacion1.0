e894cfc55c5058c20b3b08eef398baa9235df21c
Mille <Mille@DESKTOP-0BE4567>
2019-05-04 10:07:48 -0500

    asdasdasdasd

--- Metodossimulacion1.0/src/views/JDialogLinealCongruency.java
+++ Metodossimulacion1.0/src/views/JDialogLinealCongruency.java
@@ -1,25 +1,17 @@
 package views;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.Toolkit;
 
-import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
-import controller.Action;
-import controller.Controller;
-import uptc.com.sim.entities.LinealCongruency;
 
 public class JDialogLinealCongruency extends JDialog{
-	
-	
-	
+
+	private static final long serialVersionUID = 1L;
 	private JTableLinealCongruency jTableLinealCongruency;
-	
+
 	public JDialogLinealCongruency() {
 		this.setSize(new Dimension((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth()/3,
 				(int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight()- 
@@ -29,15 +21,13 @@ public class JDialogLinealCongruency extends JDialog{
 	}
 
 	private void init() {
-		JPanel jPanelMain = new JPanel();
-		
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane);
 		jTableLinealCongruency = new JTableLinealCongruency();
 		jTableLinealCongruency.setPreferredSize(new Dimension((int)this.getWidth()-40, this.getHeight()*3-this.getHeight()/4));
-		
-		jPanelMain.add(jTableLinealCongruency);
-		this.add(jPanelMain);
+		scrollPane.setViewportView(jTableLinealCongruency);
 	}
-	
+
 	public JTableLinealCongruency getjTableLinealCongruency() {
 		return jTableLinealCongruency;
 	}
