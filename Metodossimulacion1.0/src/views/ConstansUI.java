5ea76a2802654e4fda42e50b642cc0af4c8cb802
Mille <Mille@DESKTOP-0BE4567>
2019-05-04 22:21:19 -0500

    Agregando un dialogo para valores

--- Metodossimulacion1.0/src/views/ConstansUI.java
+++ Metodossimulacion1.0/src/views/ConstansUI.java
@@ -6,9 +6,15 @@ public class ConstansUI {
 	public static final int SIZE_WINDOW_X = (600);
 	public static final int SIZE_WINDOW_Y = (500);
 	public static final int SIZE_WINDOW_SEED_Y = (800);
+	
+	public static final int SIZE_WINDOW_DIALOG_INPUT_X = (300);
+	public static final int SIZE_WINDOW_DIALOG_INPUT_Y = (200);
+	
 	public static final String MESSAGE_TITTLE_FRAME = "Metodos de congruencias";
 	public static final String MESSAGE_TITTLE_DIALOG_MIDDLE = "Metodo Cuadrados Medios";
+	public static final String MESSAGE_TITTLE_DIALOG_INPUT_VALUES = "Ingresar valores";
 	public static final String MESSAGE_TITTLE_DIALOG_DISTRIBUTION = "Distribucion Uniforme + Prueba Chi2";
+	
 	
 	public static final Color COLOR_FRAME = Color.decode("#FF8686");
 	public static final Color COLOR_PANEL_CENTER = Color.decode("#FF5555");
